a21099b38c16485d0beef68bab823d05e8b7b621
Douglas Putnam <douglasputnam@live.com>
2013-05-01 15:38:56 -0700

    removed validation code

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,5 @@
 class Url < ActiveRecord::Base
   attr_accessible :comment, :name, :student_id, :url
   belongs_to :student
-  validates :url, :format => {:with => /^http:\/\/.*herokuapp.com\/?$/, :message => 'Must be a Heroku sub-domain'}
+  # add validation so that only URLs that end with herokuapp.com are accepted.
 end
