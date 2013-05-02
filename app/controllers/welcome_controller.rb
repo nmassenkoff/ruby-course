06a70c8593329f27f57642a7df046dfde3a3319a
Douglas Putnam <douglasputnam@live.com>
2013-05-01 23:25:03 -0700

    fixed bug in Welcome#create

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,7 @@ class WelcomeController < ApplicationController
 
     # We will be creating a new student object to use with Register URL form
     @student = Student.new 
+    @students = Student.all
     
     respond_to do |format|
       format.html # index.html.erb
@@ -23,13 +24,21 @@ class WelcomeController < ApplicationController
     respond_to do |format|
       # add the URL to the student
       # Ruby will give the URL the correct student_id
-      if @student.save and @student.urls.create!({:name => '',:url => params[:url]})
-        format.html { redirect_to  '/urls', notice: 'Url was successfully created.' }
-        format.json { render json: @url, status: :created, location: @url }
-      else
-        # If the form doesn't validate we'll just redirect.
-        # We have to create a route in config/routes.rb for this to work
+      if !(params[:student][:course] == 'cs132a' or params[:student][:course] == 'cis113')
         format.html { redirect_to  '/error'}
+      else
+        if params[:url] =~ /^http:\/\/\w+-\w*-\d{4}\.herokuapp.com\/?$/
+          if @student.save and @student.urls.create!({:name => '',:url => params[:url]}) 
+            format.html { redirect_to  '/urls', notice: 'Url was successfully created.' }
+            format.json { render json: @url, status: :created, location: @url }
+          else
+            # If the form doesn't validate we'll just redirect.
+            # We have to create a route in config/routes.rb for this to work
+            format.html { redirect_to  '/error'}
+          end
+        else
+            format.html { redirect_to  '/error'}
+        end
       end
     end
   end
