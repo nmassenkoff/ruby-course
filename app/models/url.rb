9cc954bcd24b1d0d9036a65b925da98e6a9dae07
Nicholas Massenkoff <nmassenkoff@gmail.com>
2013-05-15 00:28:39 -0700

    Add table to index page

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,6 @@ class Url < ActiveRecord::Base
   attr_accessible :comment, :name, :student_id, :url
   belongs_to :student
   # add validation so that only URLs that end with herokuapp.com are accepted.
+  VALID_HEROKU_REGEX = /herokuapp\.com\z/i
+  validates :url, :presence true, format: { with: VALID_HEROKU_REGEX }
 end
